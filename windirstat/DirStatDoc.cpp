c50e8f4d432c29a66699b4add5f429751af9c8b4
harryytm <harryytm@gmail.com>
2026-03-24 11:00:15 +0000

    Extreme manual inlining

--- windirstat/DirStatDoc.cpp
+++ windirstat/DirStatDoc.cpp
@@ -1622,11 +1622,9 @@ void CDirStatDoc::OnComputeHash()
 {
     // Compute the hash in the message thread
     std::wstring hashResult;
-    const auto& items = GetAllSelected();
-    const ULONGLONG totalSize = items.front()->GetSizeLogical() / wds::Mi;
-    CProgressDlg(totalSize , false, AfxGetMainWnd(), [&](CProgressDlg* pdlg)
+    CProgressDlg(GetAllSelected().front()->GetSizeLogical() / wds::Mi, false, AfxGetMainWnd(), [&](CProgressDlg* pdlg)
     {
-        hashResult = ComputeFileHashes(items.front()->GetPath(), pdlg);
+        hashResult = ComputeFileHashes(GetAllSelected().front()->GetPath(), pdlg);
     }).DoModal();
 
     if (!hashResult.empty())
