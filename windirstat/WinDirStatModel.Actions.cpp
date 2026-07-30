847711db505eb9b67323b0a9626a45bee6244106
harryytm <harryytm@gmail.com>
2026-07-30 14:14:53 +0100

    Command Handler Refactoring

--- windirstat/WinDirStatModel.Actions.cpp
+++ windirstat/WinDirStatModel.Actions.cpp
@@ -1432,8 +1432,8 @@ void CWinDirStatModel::OnToolsSetDates()
 
 void CWinDirStatModel::OnSearchEmptyFolders()
 {
-    const auto& roots = GetAllSelected();
-    if (roots.empty()) return;
-    CFileSearchControl::Get()->SearchEmptyFolders(roots);
+    const std::vector<CItem*>& items = GetAllSelected();
+    if (items.empty()) return;
+    CFileSearchControl::Get()->SearchEmptyFolders(items);
     CMainFrame::Get()->GetFileTabbedView()->SetActiveSearchView();
 }
